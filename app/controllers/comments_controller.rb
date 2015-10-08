de29d3f61c0cccb69e089aefa5c1a60bedbc156e
José Mota <jose@josemota.net>
2015-10-08 14:51:56 +0100

    Make comments possible to enter.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,9 @@ class CommentsController < ApplicationController
     @comment = @task.comments.build(comment_params)
 
     if @comment.save
-      redirect_to @task, notice: "Comment posted."
+      redirect_to [ @task.project, @task ], notice: "Comment posted."
     else
-      redirect_to @task, alert: "Comment not posted."
+      redirect_to [ @task.project, @task ], alert: "Comment not posted."
     end
   end
 
